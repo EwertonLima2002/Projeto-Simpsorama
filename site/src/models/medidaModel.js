d3ce9886aa164101730c4e0e25b345707552cedd
EwertonLima2002 <ewerton.lima860@sptech.school>
2023-04-29 11:17:53 -0300

    Grafico funcionado perfeitamente :)

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -5,23 +5,13 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top ${limite_linhas}
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        momento,
-                        FORMAT(momento, 'HH:mm:ss') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc`;
-    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
+        instrucaoSql = `select count(usuario.fkpersonagem) as voto , personagem.nome as personagem, usuario. *   
+        from usuario join personagem on idpersonagem= fkpersonagem group by personagem;`;
+
+    }
+     else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select count(usuario.fkpersonagem) as voto , personagem.nome as personagem, usuario. *   
+        from usuario join personagem on idpersonagem= fkpersonagem group by personagem;`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
